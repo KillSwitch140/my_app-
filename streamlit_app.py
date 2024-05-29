90b03fc928ece4f4fef78ab24767b4d96e11281b
KillSwitch140 <124943710+KillSwitch140@users.noreply.github.com>
2024-05-29 15:53:51 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,19 +1,11 @@
 import streamlit as st
-
-
-with st.sidebar:
-    "[View the source code](https://github.com/streamlit/llm-examples/blob/main/pages/1_File_Q%26A.py)"
-    "[![Open in GitHub Codespaces](https://github.com/codespaces/badge.svg)](https://codespaces.new/streamlit/llm-examples?quickstart=1)"
+import csv
 
 st.title("Interview setup page for Interviewer")
 
 uploaded_file = st.file_uploader("Upload interview file", type=("csv"))
 
-question = st.text_input(
-    "Ask something about the article",
-    placeholder="Can you give me a short summary?",
-    disabled=not uploaded_file,
-)
-
-
+if st.button('Submit', key='submit_button'):
+    reader = csv.reader(csv_file)
+        
 
